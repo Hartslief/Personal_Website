e77cf0dd4af4a443aaaba94e2f68574067d9d762
Hartslief <calebhartslief@gmail.com>
2026-04-04 22:30:13 +0200

    updated homescreen for mobile kekw

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,14 @@ export const metadata: Metadata = {
 export default function Home() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-row items-center gap-3 py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <div className="text-lg font-mono">I am </div>
-                <div className="text-lg font-mono mouse select-none">
+            <main className="flex min-h-screen w-full max-w-3xl flex-row justify-center items-center gap-3 bg-white dark:bg-black">
+                <div className="sm:text-xs lg:text-lg font-mono">I am </div>
+                <div className="sm:text-xs lg:text-lg font-mono mouse select-none">
                     <AgeTimer />
                 </div>
-                <div className="text-lg font-mono">years old.</div>
+                <div className="sm:text-xs lg:text-lg font-mono">
+                    years old.
+                </div>
             </main>
         </div>
     );
